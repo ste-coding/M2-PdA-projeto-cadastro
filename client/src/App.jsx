a249e47d61c7d4e3c9183f517ef316803648f544
Stéphanie Cândido <stephanie.candido42@gmail.com>
2024-03-10 20:51:26 -0300

    feat:add import statement for App.css file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Items from './Items'
 import CreateItem from './CreateItem'
@@ -10,15 +10,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
       <BrowserRouter>
         <Routes>
-          <Route path = '/' element={<Items/>}></Route>
-          <Route path = '/create' element={<CreateItem/>}></Route>
-          <Route path = '/update/:id' element={<UpdateItem/>}></Route>
+          <Route path='/' element={<Items />} />
+          <Route path='/create' element={<CreateItem />} />
+          <Route path='/update/:id' element={<UpdateItem />} />
         </Routes>
       </BrowserRouter>
-    </div>
   )
 }
 
