cc513b9029e9d801a1ab04bf7209df8bb7d3f541
Stéphanie Cândido <stephanie.candido42@gmail.com>
2024-03-11 00:58:21 -0300

    fix:resolve dotenv configuration error

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const ItemModel = require('./models/Items.js')
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express()
 app.use(cors())
